74aa58c38c10421a8bee5c5e428c9c9b9e1a4468
Siu <siuc@protonmail.com>
2022-04-07 14:35:23 -0400

    update length

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,27 @@ export class AppComponent {
   includeLowerLetter = true;
   includeNumber = true;
   includeSymbol = true;
+  password = '';
+
+  updateLength = (e: KeyboardEvent) => {
+    const newInput = (e.target as HTMLInputElement).value;
+    const inputInt = parseInt(newInput);
+    const isNumber = isNaN(inputInt);
+    if (!isNumber) this.length = inputInt;
+  };
 
   toggleUpperLetter = () => {
     this.includeUpperLetter = !this.includeUpperLetter;
   };
+
   toggleLowerLetter = () => {
     this.includeLowerLetter = !this.includeLowerLetter;
   };
+
   toggleNumber = () => {
     this.includeNumber = !this.includeNumber;
   };
+
   toggleSymbol = () => {
     this.includeSymbol = !this.includeSymbol;
   };
