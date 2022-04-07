07c1e485a720a8661bdaa1f6d4a8eccf700286ec
Siu <siuc@protonmail.com>
2022-04-07 14:06:11 -0400

    add first function for toggling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'password-generator';
+  length = 0;
+  includeUpperLetter = true;
+  includeLowerLetter = true;
+  includeNumber = true;
+  includeSymbol = true;
+
+  toggleUpperLetter = () => {
+    console.log('CLICK');
+    this.includeUpperLetter = !this.includeUpperLetter;
+  };
 }
